52163d1fa3f74696821710551b773fb50ad2c390
Daniela Martinez <daniela@Danielas-Personal-MacBook.local>
2025-07-03 22:55:20 -0500

    found bug while fixing git history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
     console.log("filtering advocates...");
 
     const filteredAdvocates = advocates.filter((advocate: Advocate) => {
-      const lowerCaseTerm = searchTerm.toLowerCase()
+      const lowerCaseTerm = searchedTerm.toLowerCase()
 
       return (
         advocate.firstName.toLowerCase().includes(lowerCaseTerm) ||
@@ -70,7 +70,7 @@ export default function Home() {
         <div className="loading-screen">
           <button type="button" className="flex text-green-800 w-1/2 items-center justify-center" disabled>
             <svg className="mr-3 animate-spin -ml-1 mr-3 h-5 w-5 text-green-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
             Fetching Advocates
@@ -81,7 +81,7 @@ export default function Home() {
         <>
           <div className="flex w-full">
             <label className= "hidden" htmlFor="search">Search</label>
-            <input className="w-4/12 py-4 px-4" id="search" placeholder="Search For Advocates" style={{ border: "1px solid black" }} onChange={onChange} />
+            <input className="w-4/12 py-4 px-4" id="search" placeholder="Search For Advocates" style={{ border: "1px solid black" }} onChange={onChange} value={searchTerm} />
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded ml-3" onClick={onClick}>Reset Search</button>
           </div>
           <br />
